04fbfca7607543022f852c5ce076079b0e4013b1
henriqueoldev <195778740+henriqueoldev@users.noreply.github.com>
2026-05-07 10:20:45 -0300

    Fixed ts variable bug

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 export default function Btn({Text, Icon, Size}: Props) {
-    Text = Text.toUpperCase()
+    Text = Text?.toUpperCase()
     return (
         <button className="bg-accent font-bold hover:cursor-pointer flex p-2 rounded-xl items-center justify-center gap-1 border-accent border-3 hover:bg-white hover:text-accent text-white transition duration-100">
             {Text}
